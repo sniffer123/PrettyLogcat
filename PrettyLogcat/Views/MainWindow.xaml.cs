e646898e0a7ae6b8d10fdaaffe3ec6071a47ee92
Trudu Laurent <laurent@trudu.com>
2025-11-10 17:35:00 +0100

    Changed the toggle button to checkboxes to make the auto scroll and word wrap more visible. Implemented the autoscroll feature.

--- PrettyLogcat/Views/MainWindow.xaml.cs
+++ PrettyLogcat/Views/MainWindow.xaml.cs
@@ -1,11 +1,15 @@
 using System;
-using PrettyLogcat.ViewModels;
+using System.Collections.Specialized;
 using System.Windows;
+using System.Windows.Controls;
+using PrettyLogcat.ViewModels;
 
 namespace PrettyLogcat.Views
 {
     public partial class MainWindow : Window
     {
+        private readonly MainViewModel _viewModel;
+
         public MainWindow(MainViewModel viewModel)
         {
             if (viewModel == null)
@@ -13,6 +17,33 @@ namespace PrettyLogcat.Views
 
             InitializeComponent();
             DataContext = viewModel;
+            _viewModel = viewModel;
+
+            // Subscribe to collection changes for auto-scroll
+            _viewModel.FilteredLogs.CollectionChanged += OnFilteredLogsChanged;
+        }
+
+        private void OnFilteredLogsChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+            // Only auto-scroll if enabled and items were added
+            if (_viewModel.AutoScroll && e.Action == NotifyCollectionChangedAction.Add)
+            {
+                // Scroll to the bottom of the DataGrid
+                Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    if (LogDataGrid.Items.Count > 0)
+                    {
+                        LogDataGrid.ScrollIntoView(LogDataGrid.Items[LogDataGrid.Items.Count - 1]);
+                    }
+                }), System.Windows.Threading.DispatcherPriority.Background);
+            }
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            // Unsubscribe from events
+            _viewModel.FilteredLogs.CollectionChanged -= OnFilteredLogsChanged;
+            base.OnClosed(e);
         }
     }
 }
